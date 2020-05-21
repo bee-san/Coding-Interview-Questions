5bbeddc8282948c0b5e7e02bb524609dd9559657
Brandon <brandonskerritt51@gmail.com>
2020-05-21 13:40:20 +0100

    Completed leetcode

---
+++ leetcode/top_interview_questions/array/docsRemoveDuplicates.md
@@ -0,0 +1,2 @@
+# Documentation
+:wq
--- leetcode/top_interview_questions/array/removeDuplicatesFromSortedArray.py
+++ leetcode/top_interview_questions/array/removeDuplicatesFromSortedArray.py
@@ -1,3 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
+        counter = 0
+        # We use while loop to try and cut down on 2 loops
+        while counter < (len(nums) - 1):
+            # if current counter is the same as the next one
+            # removr current
+            if nums[counter] == nums[counter + 1]:
+                nums.remove(nums[counter])
+            else:
+                # else they are not the same, so counter +=1 
+                counter += 1
+        return len(nums)
